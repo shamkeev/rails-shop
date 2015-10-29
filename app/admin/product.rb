2b0ae44ddfa0b96314c976fef6083f203d85b60b
shamkeev <shamkeyev@mail.ru>
2015-10-29 20:32:19 +0600

    Added active admin configs to product model to work with paperclip. Added default image missing.png

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,27 +1,45 @@
 ActiveAdmin.register Product do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-permit_params :title, :description, :price, :category_id
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
+  permit_params :category_id, :title, :description, :price, :image
 
-    # Это будет показывать только указанные колонки в админке
-
-    index do
-      selectable_column
-      column "Title" do |product|
-        link_to product.title, admin_product_path(product)
-      end
-
-      column :price
+  index do
+    selectable_column
+    id_column
+    column :image do |product|
+      image_tag product.image.url(:thumb)
     end
+    column :title do |product|
+      link_to product.title, admin_product_path(product)
+    end
+    column :price
+    column :category
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :category
+      f.input :title
+      f.input :description
+      f.input :price
+      #
+      f.input :image, :as => :file, :hint => image_tag(f.object.image.url(:thumb))
+
+      f.actions
+    end
+  end
+
+  show do
+    attributes_table do
+      row :image do |product|
+        image_tag product.image.url(:medium)
+      end
+      row :title
+      row :category
+      row :price
+      row :description
+    end
+    active_admin_comments
+  end
 
 end
