3056f66639f6cff6a4ae8eabf6661f9ee58740da
shamkeev <shamkeyev@mail.ru>
2015-11-05 19:26:59 +0600

    Changed view to work with paperclip. Added some fixtures.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,3 @@
-def copy_image_fixture(file_name, id)
-  dir = Rails.root.join('public', 'uploads')
-  Dir.mkdir(dir) unless File.directory?(dir)
-  FileUtils.cp(file_name, dir.join(id.to_s))
-end
 
 cpus_category = Category.create(name: 'CPUs')
 motherboards_category = Category.create(name: 'Motherboards')
@@ -13,17 +8,23 @@ fixtures_category = Category.create(name: 'Fixtures')
 
 fixtures_path = Rails.root.join('app', 'assets', 'images', 'fixtures')
 
-cpu = Product.create(title: 'Intel Core i7', price: 300, description: Faker::Lorem.paragraph, category: cpus_category)
-copy_image_fixture(fixtures_path.join('cpu.jpg'), cpu.id)
+cpu = Product.create(
+    title: 'Intel Core i7',
+    price: 300,
+    description: Faker::Lorem.paragraph,
+    category: cpus_category,
+    image: File.new(fixtures_path.join('cpu.jpg'))
+)
 
-ram = Product.create(title: '4GB DDR3 RAM', price: 40, description: Faker::Lorem.paragraph, category: ram_category)
-copy_image_fixture(fixtures_path.join('ram.jpg'), ram.id)
 
-hdd = Product.create(title: '1TB Seagate HDD', price: 60, description: Faker::Lorem.paragraph, category: hdd_category)
-copy_image_fixture(fixtures_path.join('hdd.jpg'), hdd.id)
+ram = Product.create(title: '4GB DDR3 RAM', price: 40, description: Faker::Lorem.paragraph, category: ram_category,image: File.new(fixtures_path.join('ram.jpg')))
 
-mb = Product.create(title: 'Asus P5Q3', price: 120, description: Faker::Lorem.paragraph, category: motherboards_category)
-copy_image_fixture(fixtures_path.join('mb.jpg'), mb.id)
+
+hdd = Product.create(title: '1TB Seagate HDD', price: 60, description: Faker::Lorem.paragraph, category: hdd_category,image: File.new(fixtures_path.join('hdd.jpg')))
+
+
+mb = Product.create(title: 'Asus P5Q3', price: 120, description: Faker::Lorem.paragraph, category: motherboards_category,image: File.new(fixtures_path.join('mb.jpg')))
+
 
 50.times do
   Product.create(title: Faker::Commerce.product_name,
